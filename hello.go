8b2ca458e6f6819dc2f07226ed5126eaf3e428c7
knoeks <poviliuxg@gmail.com>
2024-04-19 19:57:51 +0300

    add outside var

--- hello.go
+++ hello.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 )
 
+var outside string = "outside the \"main()\""
+
 func main() {
-	var x int = 10
-	var y float32 = 19.99
+	var x int = 2
+	var y float32 = 2.999
 	var z float32 = float32(x)
 
-	fmt.Printf("%.3f", z+y)
+	fmt.Printf("%.3f ", z+y)
+
+	gatve := "verkių g."
+	fmt.Println(gatve)
+	println(outside)
 }
